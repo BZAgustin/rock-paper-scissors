8695f7e4ed063b92d17b644285f364f0a372cec3
BZAgustin <abustoszanelli@gmail.com>
2022-04-02 11:46:56 -0300

    - Add UI to the project (style.css) - Add basic script for playing using UI (script.js)

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-function computerPlay() {
+// Score variables
+let myScore = 0;
+let botScore = 0;
+
+// Target the 3 main game buttons and computer buttons
+const pRock = document.querySelector('#pRock');
+const pPaper = document.querySelector('#pPaper');
+const pScissors = document.querySelector('#pScissors');
+
+const cRock = document.querySelector('#cRock');
+const cPaper= document.querySelector('#cPaper');
+const cScissors = document.querySelector('#cScissors');
+
+// Target notifications and game score
+const score = document.querySelector('#score');
+const notif = document.querySelector('.notification');
+
+// Listeners
+pRock.addEventListener('click', (e) => {
+    playRound(e);
+});
+
+pPaper.addEventListener('click', (e) => {
+    playRound(e);
+
+});
+
+pScissors.addEventListener('click', (e) => {
+    playRound(e);
+});
+
+// Randomly makes a choice for the bot
+function botPickNumber() {
     let choice = Math.ceil(Math.random() * 3);
     if(choice === 1) {
         return "rock";
@@ -7,37 +39,111 @@ function computerPlay() {
     } else return "scissors";
 };
 
-function playerPlay() {
-    let play = prompt("Rock, paper or scissors?").toLowerCase();
-    return play;
+// Triggered whenever the player clicks on a choice
+function playRound(e) {
+    let playerChoice = e.target.id;
+    let botChoice = botPickNumber();
+
+    highlightChoices(e, botChoice);
+
+    if(playerChoice === 'pRock') {
+        if(botChoice === 'paper') {
+            botScore++;
+            score.textContent = `You  ${myScore} - ${botScore}  Computer`;
+            notif.textContent = "Player loses. Paper beats Rock";
+        } else if(botChoice === 'scissors') {
+            myScore++;
+            score.textContent = `You  ${myScore} - ${botScore}  Computer`;
+            notif.textContent = "Player wins. Rock beats Scissors";
+        } else {
+            notif.textContent = "It's a Tie";
+        }
+    } else if(playerChoice === 'pPaper') {
+        if(botChoice === 'scissors') {
+            botScore++;
+            score.textContent = `You  ${myScore} - ${botScore}  Computer`;
+            notif.textContent = "Player loses. Scissors beat Paper";
+        } else if(botChoice === 'rock') {
+            myScore++;
+            score.textContent = `You  ${myScore} - ${botScore}  Computer`;
+            notif.textContent = "Player wins. Paper beats Rock";
+        } else {
+            notif.textContent = "It's a Tie";
+        }
+    } else {
+        if(botChoice === 'rock') {
+            botScore++;
+            score.textContent = `You  ${myScore} - ${botScore}  Computer`;
+            notif.textContent = "Player loses. Rock beats Scissors";
+        } else if(botChoice === 'paper') {
+            myScore++;
+            score.textContent = `You  ${myScore} - ${botScore}  Computer`;
+            notif.textContent = "Player wins. Scissors beat Paper";
+        } else {
+            notif.textContent = "It's a Tie";
+        }
+    }
+
+    if(myScore === 5) {
+       notif.textContent = "Player is the final winner!";
+       myScore = 0;
+       botScore = 0;
+       score.textContent = `0 - 0`;
+    } else if(botScore === 5) {
+       notif.textContent = "Computer is the final winner!";
+       myScore = 0;
+       botScore = 0;
+       score.textContent = `0 - 0`;
+    }
+
+    refreshItems();
 }
 
-let player = playerPlay();
-let bot = computerPlay();
+// function checkWinner(myObj, botObj, winner) {
+//     console.log(`${winner} wins. `);
+// }
 
-console.log(`Player's choice: ${player}`);
-console.log(`Computer's choice: ${bot}`);
-
-function playRound(playerSelection, BotSelection) {
-    if(playerSelection === "rock") {
-        if(BotSelection === "paper") {
-            return "You Lose! Paper beats Rock";
-        } else if(BotSelection === "scissors") {
-            return "You Win! Rock beats Scissors";
-        } else return "It's a tie!";
-    } else if(playerSelection === "paper") {
-        if(BotSelection === "scissors") {
-            return "You Lose! Scissors beats Paper";
-        } else if(BotSelection === "rock") {
-            return "You Win! Paper beats Rock";
-        } else return "It's a tie!";
-    } else {
-        if(BotSelection === "rock") {
-            return "You Lose! Rock beats Scissors";
-        } else if(BotSelection === "paper") {
-            return "You Win! Scissors beats Paper";
-        } else return "It's a tie!";
+function highlightChoices(e, botChoice) {
+    if(e.target.id === 'pRock') {
+        pRock.style.border = "3px solid #fd6017";
+        pRock.style.backgroundColor = "#ffcc6e";
+    } else if(e.target.id === 'pPaper') {
+        pPaper.style.border = "3px solid #fd6017";
+        pPaper.style.backgroundColor = "#ffcc6e";
+    } else if(e.target.id === 'pScissors') {
+        pScissors.style.border = "3px solid #fd6017";
+        pScissors.style.backgroundColor = "#ffcc6e";
     }
-};
 
-console.log(playRound(player, bot));
+    // a bit of delay for the bot choice
+   setTimeout(() => {
+    if(botChoice === 'rock') {
+        cRock.style.border = "3px solid #fd6017";
+        cRock.style.backgroundColor = "#ffcc6e";
+    } else if(botChoice === 'paper') {
+        cPaper.style.border = "3px solid #fd6017";
+        cPaper.style.backgroundColor = "#ffcc6e";
+    } else if(botChoice === 'scissors') {
+        cScissors.style.border = "3px solid #fd6017";
+        cScissors.style.backgroundColor = "#ffcc6e";
+    }
+   }, 200);
+}
+
+function refreshItems() {
+    setTimeout(() => {
+        pRock.style.border = "3px solid #ff8410";
+        pPaper.style.border = "3px solid #ff8410";
+        pScissors.style.border = "3px solid #ff8410";
+        pRock.style.backgroundColor = "white";
+        pPaper.style.backgroundColor = "white"
+        pScissors.style.backgroundColor = "white"
+
+        cRock.style.border = "3px solid #ff8410";
+        cPaper.style.border = "3px solid #ff8410";
+        cScissors.style.border = "3px solid #ff8410";
+        cRock.style.backgroundColor = "white";
+        cPaper.style.backgroundColor = "white";
+        cScissors.style.backgroundColor = "white";
+    }, 2000);
+};
